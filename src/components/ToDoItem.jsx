08beff163366a24130c6d33c34f0a58c91914da4
Prakhar Srivastava <50711734+prakhar-srivastava-1@users.noreply.github.com>
2022-02-28 06:51:29 +0530

    add logic to strikethrough the completed tasks On clicking a particular list item (task), it should be crossed. On a re-click, it should be uncrossed.

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function ToDoItem(props) {
-  return <li>{props.toDoItemText}</li>;
+  const [styleCrossed, toggleCrossed] = React.useState("none");
+
+  function updateState() {
+    const newState = styleCrossed === "none" ? "line-through" : "none";
+    toggleCrossed(newState);
+  }
+
+  return (
+    <div onClick={updateState}>
+      <li style={{ textDecoration: styleCrossed }}>{props.toDoItemText}</li>
+    </div>
+  );
 }
 
 export default ToDoItem;
