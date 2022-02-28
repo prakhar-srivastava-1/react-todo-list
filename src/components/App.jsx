dd9cd491fe994e1b61d04cbcdb9e40eb8b711924
Prakhar Srivastava <50711734+prakhar-srivastava-1@users.noreply.github.com>
2022-02-28 05:43:01 +0530

    complete to-do list base version This is just the front-end with no linked back-end. Notes are lost as soon as the page is refreshed.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,39 @@
 import React from "react";
 
 function App() {
+  // start using state
+  const [item, setItem] = React.useState("");
+  const [listItems, setListItem] = React.useState([]);
+
+  function captureItem(event) {
+    const { value } = event.target;
+    setItem(value);
+  }
+
+  function addToList() {
+    setListItem((prevValue) => {
+      console.log([...prevValue, item]);
+      return [...prevValue, item];
+    });
+  }
+
+  function addListTags(item) {
+    return <li>{item}</li>;
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={captureItem} type="text" />
+        <button onClick={addToList}>
           <span>Add</span>
         </button>
       </div>
       <div>
-        <ul>
-          <li>A Item </li>
-        </ul>
+        <ul>{listItems.map(addListTags)}</ul>
       </div>
     </div>
   );
